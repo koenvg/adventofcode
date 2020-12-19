56c5b05581afb94fd28e0d60cb593ed0c430d4d2
Koen Van Geert <koen.van.geert@gmail.com>
2020-12-19 10:57:39 +0100

    Day 18 part 2

--- day18/src/main.rs
+++ day18/src/main.rs
@@ -1,6 +1,6 @@
 use std::fs;
 
-fn parse_expression(expression: &str) -> i64 {
+fn parse_expression_part_1(expression: &str) -> i64 {
     let mut result: i64 = 0;
     let mut right = String::new();
     let mut operator = '+';
@@ -19,7 +19,7 @@ fn parse_expression(expression: &str) -> i64 {
             ')' => {
                 level -= 1;
                 if level == 0 {
-                    let parsed_result = parse_expression(&right).to_string();
+                    let parsed_result = parse_expression_part_1(&right).to_string();
                     println!("Parsing {} result {} ", right, parsed_result);
                     right = parsed_result;
                 } else {
@@ -66,7 +66,101 @@ fn part1() {
 
     let mut sum = 0;
     for line in content.lines() {
-        let result = parse_expression(&format!("{}", line));
+        let result = parse_expression_part_1(&format!("{}", line));
+
+        println!("{} = {}", line, result);
+        sum += result;
+    }
+
+    println!("Part1: {}", sum);
+}
+
+fn parse_expression_part_2(expression: &str) -> i64 {
+    let mut result: i64 = 0;
+    let mut right = String::new();
+    let mut operator = '+';
+
+    let mut level = 0;
+
+    let mut index = 0;
+    for c in format!("{} ", expression).chars() {
+        index += 1;
+        match c {
+            '(' => {
+                if level > 0 {
+                    right.push(c);
+                }
+                level += 1;
+                continue;
+            }
+            ')' => {
+                level -= 1;
+                if level == 0 {
+                    let parsed_result = parse_expression_part_2(&right).to_string();
+                    println!("Parsing {} result {} ", right, parsed_result);
+                    right = parsed_result;
+                } else {
+                    right.push(c);
+                }
+                continue;
+            }
+            '*' => {
+                if level == 0 {
+                    let parsed_result = parse_expression_part_2(&expression[index + 1..]);
+                    println!(
+                        "Parsing {} result {} ",
+                        &expression[index + 1..],
+                        parsed_result
+                    );
+                    result = result * parsed_result;
+                    break;
+                }
+                if level > 0 {
+                    right.push(c);
+                    continue;
+                }
+            }
+            _ => {
+                if level > 0 {
+                    right.push(c);
+                    continue;
+                }
+            }
+        }
+        match c {
+            ' ' => {
+                if right.len() == 0 {
+                    continue;
+                }
+                let right_number: i64 = right.parse().unwrap();
+
+                match operator {
+                    '+' => {
+                        result = result + right_number;
+                    }
+                    '*' => result = result * right_number,
+                    _ => {}
+                }
+                right = String::new();
+            }
+            '+' | '*' => {
+                operator = c;
+            }
+            _ => {
+                right.push(c);
+            }
+        }
+    }
+
+    return result;
+}
+
+fn part2() {
+    let content = fs::read_to_string("./src/input.txt").unwrap();
+
+    let mut sum = 0;
+    for line in content.lines() {
+        let result = parse_expression_part_2(&format!("{}", line));
 
         println!("{} = {}", line, result);
         sum += result;
@@ -75,5 +169,6 @@ fn part1() {
     println!("Part1: {}", sum);
 }
 fn main() {
-    part1();
+    // part1();
+    part2();
 }
